8d85e3d2e0ebeb73865e1ee4164d4b91ffd595a6
Kevin Gonzalez <kejogo.dev@outlook.com>
2025-02-27 11:13:01 -0500

    Add .env file and chatbot.py script

---
+++ chatbot.py
@@ -0,0 +1,79 @@
+from google import genai
+import os
+import panel as pn
+from dotenv import load_dotenv, find_dotenv
+_ = load_dotenv(find_dotenv())
+
+pn.extension()
+
+panels = []
+context = []
+
+
+sys_sheldon_message = """Eres un agente conversacional que parodia a Sheldon Cooper de The Big Bang Theory, cada vez que el usuario te haga una pregunta, debes responder con una frase que Sheldon diría en la serie. Por ejemplo, si el usuario te pregunta 'Quien eres?', tu respuesta podría ser 'Soy un físico teórico, especializado en física cuántica. ¿Quién eres tú?'. Sin embargo, si pregunta algo más complejo como 'Cual es el sentido de la vida?', tu respuesta podría ser 'La vida es una cosa compleja, y no se puede reducir a una simple pregunta.'. Manten el tono cretino y egocentrico que lo caracterizan, menospreciando o minimizando al usuario con comentarios propios de Sheldon Cooper del tipo "Tú opinion es irrelevante." o "No gracias, aborresco el contacto fisico en cualquiera de sus posibles variantes.".
+No limites las posibles respuestas a una serie de instrucciones dadas por este mensaje, se creativo con las respuestas al puro estilo de Sheldon Cooper demostrando siempre sentirse superior a la raza humana, carente de capacidades sociales efectivas siendo imprudente en la comunicacion. Debes formatear las respuestas mostrando los mensajes del usuario y las respuestas de Sheldon en líneas separadas, por ejemplo:
+<Kevin>: Quien eres?
+<Gemini>: Soy un físico teórico, especializado en física cuántica. ¿Quién eres tú?
+"""
+
+client = genai.Client(api_key=os.getenv('API_KEY'))
+
+sheldon_chat = client.chats.create(model='gemini-2.0-flash', config={"system_instruction": sys_sheldon_message,
+                                                                     "temperature": 1})
+
+input = pn.widgets.TextInput(name="Habla con Sheldon Cooper", placeholder="Escribe tu mensaje aqui")
+button = pn.widgets.Button(name="Enviar")
+conversation_panel = pn.Column(*panels)
+
+# Función de callback que se ejecutará SOLO cuando se haga clic en el botón
+def on_button_click(_):
+    prompt = input.value
+    if not prompt.strip():
+        return
+    
+    input.value = ""
+    
+    # Enviar el mensaje a Gemini
+    res = sheldon_chat.send_message(prompt).text
+    
+    # Actualizar los paneles para mostrar la conversación
+    panels.append(pn.Row("Tú:", pn.pane.Markdown(prompt, width=600)))
+    panels.append(pn.Row("Sheldon:", pn.pane.Markdown(res, width=600)))
+    
+    # Actualizar el panel de conversación
+    conversation_panel.objects = panels
+
+# Asignar la función al evento de clic del botón
+button.on_click(on_button_click)
+
+# Crear la interfaz
+interface = pn.Column(
+    input, 
+    button,
+    conversation_panel
+)
+interface.show()
+
+# print()
+# print("\nChat de Sheldon:")
+# print(chat_with_sheldon_chatbot("Holis, como estas?"))
+# print(chat_with_sheldon_chatbot("Que haces?"))
+# print(chat_with_sheldon_chatbot("Me llamo Kevin, cómo te llamas tú?"))
+# print(chat_with_sheldon_chatbot("Sabes mi nombre?"))
+# print(chat_with_sheldon_chatbot("Sabes como puedo crecer de estatura facilmente?"))
+
+
+# sys_mario_message = """Eres un agente conversacional que parodia a Sheldon Cooper de The Big Bang Theory, cada vez que el usuario te haga una pregunta, debes responder con una frase que Mario diría en el videojuego. Por ejemplo, si el usuario te pregunta 'Quien eres?', tu respuesta podría ser 'It's me, Mario! Yujuuu!', sin embargo si pregunta algo mas complejo como 'Cual es el sentido de la vida?', tu respuesta podría ser 'Mamma mia! No lo sé, preguntale a mi hermano. Let's Go!'. Debes formatear las respuestas mostrando los mensajes del usuario y las respuestas de Mario en lineas separadas, por ejemplo:
+# <Kevin>: Quien eres?
+# <Gemini>: It's me, Mario! Yujuuu!
+# """
+
+
+# chat = client.chats.create(model='gemini-2.0-flash')
+# mario_chat = client.chats.create(model='gemini-2.0-flash', config={"system_instruction": sys_mario_message})
+
+# def chat_with_chatbot(input):
+#     return chat.send_message(input).text
+
+# def chat_with_mario_chatbot(input):
+#     return mario_chat.send_message(input).text
